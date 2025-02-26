7e2da8028cf3fbb7b69a68aa43e7a09fb870e871
Everton Godoy <everton.mgodoy@gmail.com>
2025-02-26 09:59:01 -0300

    add methods to receive token when call recuperar/todos using annotation @EscopoNecessario

--- src/main/java/br/senac/sp/library/LegendaryVideoLibraryApplication.java
+++ src/main/java/br/senac/sp/library/LegendaryVideoLibraryApplication.java
@@ -2,8 +2,10 @@ package br.senac.sp.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.senac.sp.security.tokens", "br.senac.sp.library"})
 public class LegendaryVideoLibraryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/senac/sp/library/controller/LegendaryVideoController.java
+++ src/main/java/br/senac/sp/library/controller/LegendaryVideoController.java
@@ -2,6 +2,7 @@ package br.senac.sp.library.controller;
 
 import br.senac.sp.library.controller.response.LegendaryVideoResponse;
 import br.senac.sp.library.usecase.LegendaryVideoUseCase;
+import br.senac.sp.security.tokens.annotation.EscopoNecessario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class LegendaryVideoController {
         this.legendaryVideoUseCase = legendaryVideoUseCase;
     }
 
+    @EscopoNecessario({"listar-filmes"})
     @GetMapping(value = "/recuperar/todos")
     public ResponseEntity<LegendaryVideoResponse> recuperarTodos() {
         logger.info("[LegendaryVideoController]-[recuperarTodos] - Recuperando todos os filmes!");
